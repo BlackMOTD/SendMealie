416a9c6f68ae3f2fa6aa6505cecd40fc9030610e
BlackMOTD <bastien.warneford@gmail.com>
2026-08-20 18:23:59 +0200

    Initial Commit

---
+++ SendMealieTests/SendMealieTests.swift
@@ -0,0 +1,17 @@
+//
+//  SendMealieTests.swift
+//  SendMealieTests
+//
+//  Created by Bastien on 20/08/2026.
+//
+
+import Testing
+@testable import SendMealie
+
+struct SendMealieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
